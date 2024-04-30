af7ccaa385ccbf4ab002510ec2e01acadf23a099
JustinMaina <justinmwangi58@gmail.com>
2024-04-30 13:35:58 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => (
+        // Each <a> element with a unique key prop
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,18 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
+import user from '../data/user';
 
-function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+function ProjectList() {
+ return (
+    <div>
+      {user.projects.map(project => (
+        <ProjectItem
+          key={project.id}
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
   );
 }
